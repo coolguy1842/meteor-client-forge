9e9f17f1c5fddf8d8fe3ad26c79bea797cbebd53
coolguy1842 <coolio4691.gaming@gmail.com>
2024-04-05 00:38:12 +1000

    bettertooltips working (fishpeek doesnt work)

--- src/main/java/meteordevelopment/meteorclient/mixin/DrawContextMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/DrawContextMixin.java
@@ -5,21 +5,39 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.tooltip.HoveredTooltipPositioner;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.client.gui.tooltip.TooltipPositioner;
+import net.minecraft.client.item.TooltipData;
+import net.minecraft.text.Text;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import meteordevelopment.meteorclient.utils.tooltip.MeteorTooltipData;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Mixin(value = DrawContext.class)
-public class DrawContextMixin {
-    // TODO tooltips
-    // @Inject(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;Ljava/util/Optional;II)V", at = @At(value = "INVOKE", target = "Ljava/util/Optional;ifPresent(Ljava/util/function/Consumer;)V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
-    // private void onDrawTooltip(TextRenderer textRenderer, List<Text> text, Optional<TooltipData> data, int x, int y, CallbackInfo ci, List<TooltipComponent> list) {
-    //     if (data.isPresent() && data.get() instanceof MeteorTooltipData meteorTooltipData)
-    //         list.add(meteorTooltipData.getComponent());
-    // }
+public abstract class DrawContextMixin {
+    @Shadow
+    protected abstract void drawTooltip(TextRenderer textRenderer, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner);
 
-    // @ModifyReceiver(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;Ljava/util/Optional;II)V", at = @At(value = "INVOKE", target = "Ljava/util/Optional;ifPresent(Ljava/util/function/Consumer;)V"))
-    // private Optional<TooltipData> onDrawTooltip_modifyIfPresentReceiver(Optional<TooltipData> data, Consumer<TooltipData> consumer) {
-    //     if (data.isPresent() && data.get() instanceof MeteorTooltipData) return Optional.empty();
-    //     return data;
-    // }
+    @Inject(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;Ljava/util/Optional;II)V", at = @At(value = "HEAD"), cancellable = true)
+    private void onDrawTooltip(TextRenderer textRenderer, List<Text> text, Optional<TooltipData> data, int x, int y, CallbackInfo ci) {
+        if (data.isPresent() && data.get() instanceof MeteorTooltipData meteorTooltipData) {
+            List<TooltipComponent> list = (List<TooltipComponent>)text.stream().map(Text::asOrderedText).map(TooltipComponent::of).collect(Collectors.toList());
+            list.add(meteorTooltipData.getComponent());
+
+            drawTooltip(textRenderer, list, x, y, HoveredTooltipPositioner.INSTANCE);
+            ci.cancel();
+        }
+    }
 }
